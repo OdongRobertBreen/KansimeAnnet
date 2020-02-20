3405fabfe862f62f1b80636d6732e4f5e01216e7
DELL <waigolotimothy@gmail.com>
2020-02-20 15:21:39 +0300

    my project of hello world

--- app/src/main/java/com/example/kansimeannet/MainActivity.java
+++ app/src/main/java/com/example/kansimeannet/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.kansimeannet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+MediaPlayer mySong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mySong = MediaPlayer.create(MainActivity.this,R.raw.woman);
+
+    }
+    public void playIT(View v){
+mySong.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mySong.pause();
     }
 }
