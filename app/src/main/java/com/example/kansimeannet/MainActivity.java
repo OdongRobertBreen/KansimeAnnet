efcd32f9da34ab176be1239b2ef265683049b561
DELL <waigolotimothy@gmail.com>
2020-02-25 16:33:08 +0300

    my project of hello world

--- app/src/main/java/com/example/kansimeannet/MainActivity.java
+++ app/src/main/java/com/example/kansimeannet/MainActivity.java
@@ -3,17 +3,29 @@ package com.example.kansimeannet;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 MediaPlayer mySong;
+    Button buttonnext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mySong = MediaPlayer.create(MainActivity.this,R.raw.woman);
+        buttonnext = findViewById(R.id.button_next);
+        buttonnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
     public void playIT(View v){
